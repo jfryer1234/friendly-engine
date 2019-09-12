e9d70484121e9baf83d1e41488de2e2eeb6827ba
julie <jfryer8726@gmail.com>
2019-09-12 14:57:33 -0400

    test

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,23 @@
+class PlantsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render json: Plant.all
+  end
+
+  def show
+      render json: Plant.find(params["id"])
+  end
+
+  def create
+      render json: Plant.create(params["plant"])
+  end
+
+  def delete
+    render json: Plant.delete(params["id"])
+  end
+
+  def update
+    render json: Plant.update(params["id"], params["post"])
+  end
+end
