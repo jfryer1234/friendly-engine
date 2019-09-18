6f7bc56053a89e5a88f5f0ac6e92185c55010e37
julie <jfryer8726@gmail.com>
2019-09-18 11:38:22 -0400

    ASdfgaS

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # root route
-  root 'site#index'
+  root 'main#index'
 
-  # plant routes
-  get 'api/plants', to: 'plants#index'
-  get 'api/plants/:id', to: 'plants#show'
-  post 'api/plants', to: 'plants#create'
-  delete 'api/plants/:id', to: 'plants#delete'
-  put 'api/plants/:id', to: 'plants#update'
-
-  #succulent routes
-  get 'api/succulents', to: 'succulents#index'
-  get 'api/succulents/:id', to: 'succulents#show'
-  post 'api/succulents', to: 'succulents#create'
-  delete 'api/succulents/:id', to: 'succulents#delete'
-  put 'api/succulents/:id', to: 'succulents#update'
 
 end
